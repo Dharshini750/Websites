971c070a3a0f9c041f37cd5b5c5736bcd0569af5
Dharshini R <dharshu2004rd@gmail.com>
2024-09-13 21:51:17 +0530

    Add files via upload

---
+++ es.js
@@ -0,0 +1,8 @@
+function f1() {
+    console.log("f1")
+}
+const f2 = () => {
+    console.log("f2")
+}
+f1()
+f2()
---
+++ index.js
@@ -0,0 +1,28 @@
+let number=10;
+let string="Hi";
+let boolean=false;
+let nullvalue=null;
+let undefinedvalue=undefined;
+console.log(number+" "+string+" "+boolean);
+console.log(number);
+console.log(nullvalue);
+console.log(undefinedvalue);
+let datajs={
+    name:"Rd",
+    phone:9876543210,
+    alive:true
+}
+let datajson={
+    "name":"Rd",
+    "phone":9876543210,
+    "alive":true
+}
+console.log(datajs);
+console.log(datajson);
+let dataarray=[datajs,datajson,"hi",false]
+console.log(dataarray[3])
+let Hi="hi"
+let hi="hi"
+console.log(Hi);
+console.log(hi);
+
